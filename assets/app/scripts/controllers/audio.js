ff6a2120fe1729a09cf994fa4556a6a0e7141c68
Anthony Pasquale <anthony.pasquale@smsmt.com>
2013-07-11 11:51:01 +1000

    Audio persistence for life of app

--- assets/app/scripts/controllers/audio.js
+++ assets/app/scripts/controllers/audio.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('assetsApp')
-  .controller('AudioCtrl', function ($scope, audioSvc) {
+  .controller('AudioCtrl', function ($scope, audioSvc, $rootScope) {
     /*
     $scope.awesomeThings = [
 
@@ -9,12 +9,23 @@ angular.module('assetsApp')
       'AngularJS',
       'Karma'
     ];*/
+    if (typeof $rootScope.audiofiles === 'undefined') {
+        console.log('CameraCtrl: No rootScope.image');
+        $scope.audiofiles = []
+    }
+    else
+    {
+        console.log('CameraCtrl: Found rootScope.image');
+        $scope.audiofiles = $rootScope.audiofiles
+    }
+
     $scope.captureAudio = function() {
         console.log('AudioCtrl: captureAudio') ;
         //$scope.audiofiles  =  audioSvc.captureAudio();
         audioSvc.captureAudio().then(function(audioFiles) {
             console.log('AudioCtrl: Success');
-            $scope.audiofiles = audioFiles;
+            $scope.audiofiles = $scope.audiofiles.concat(audioFiles);
+            $rootScope.audiofiles = $scope.audiofiles;
         }, function(reason) {
             console.log('AudioCtrl: Failed');
             alert('Failed: ' + reason);
--- assets/app/scripts/controllers/main.js
+++ assets/app/scripts/controllers/main.js
@@ -7,23 +7,41 @@ angular.module('assetsApp')
     'Testacular'
   ];
 
-  $scope.image1 = 'http://placehold.it/350x150';
-  $scope.image2 = 'http://placehold.it/100x100';
+    $scope.audiofiles1 = [
+      {'name': 'Audio 1', 'fullPath': '/sdcard/Audio1.3gpp'},
+      {'name': 'Audio 2', 'fullPath': '/sdcard/Audio2.3gpp'},
+      {'name': 'Audio 3', 'fullPath': '/sdcard/Audio3.3gpp'}
+    ];
+    $scope.audiofiles2 = [
+        {'name': 'Audio 4', 'fullPath': '/sdcard/Audio4.3gpp'},
+        {'name': 'Audio 5', 'fullPath': '/sdcard/Audio5.3gpp'},
+        {'name': 'Audio 6', 'fullPath': '/sdcard/Audio6.3gpp'}
+    ];
 
-    if (typeof $rootScope.image === 'undefined') {
-        console.log('MainCtrl: No rootScope.image');
-        $scope.image = $scope.image1
+    if (typeof $rootScope.afiles === 'undefined') {
+        console.log('MainCtrl: No rootScope.audiofiles');
+        $scope.audiofiles = []
     }
     else
     {
-        console.log('MainCtrl: Found rootScope.image');
-        $scope.image = $rootScope.image
+        console.log('MainCtrl: Found rootScope.audiofiles');
+        $scope.audiofiles = $rootScope.afiles
     }
 
-    $scope.swicthPhoto = function() {
-        console.log('MainCtrl: swicthPhoto');
-        $scope.image = $scope.image2;
-        $rootScope.image   = $scope.image2;
+    var arrnum = 1
+    $scope.addFiles = function() {
+        console.log('MainCtrl: switchPhoto');
+        if(arrnum == 1)   {
+            console.log('MainCtrl: pushing audiofiles1');
+            $scope.audiofiles = $scope.audiofiles.concat($scope.audiofiles1);
+            arrnum = arrnum + 1;
+        }
+        else   {
+            console.log('MainCtrl: pushing audiofiles2');
+            $scope.audiofiles = $scope.audiofiles.concat($scope.audiofiles2);
+        }
+        console.log($scope.audiofiles);
+        $rootScope.afiles = $scope.audiofiles;
     }
   
   /*geolocation.getCurrentPosition(function (position) {
